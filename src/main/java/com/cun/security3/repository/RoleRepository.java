39f59aaab44b191584ea5ce8309a5b60b6a84ad8
gustavoNull <bigwanggang@163.com>
2019-01-08 12:27:01 +0800

    Create RoleRepository.java

---
+++ src/main/java/com/cun/security3/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.cun.security3.repository;
+
+import com.cun.security3.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
