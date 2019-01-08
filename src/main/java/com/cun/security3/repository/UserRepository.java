ad73335f13108580f65b8fcd13bba70181f24b1f
gustavoNull <bigwanggang@163.com>
2019-01-08 12:27:22 +0800

    Create UserRepository.java

---
+++ src/main/java/com/cun/security3/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cun.security3.repository;
+
+import com.cun.security3.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
