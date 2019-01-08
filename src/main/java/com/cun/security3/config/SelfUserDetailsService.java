801882b5caf04b92fbdfde8279a87e2a757b42e4
gustavoNull <bigwanggang@163.com>
2019-01-08 12:23:13 +0800

    Update SelfUserDetailsService.java

--- src/main/java/com/cun/security3/config/SelfUserDetailsService.java
+++ src/main/java/com/cun/security3/config/SelfUserDetailsService.java
@@ -1,32 +1,37 @@
 package com.cun.security3.config;
 
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import com.cun.security3.entity.User;
+import com.cun.security3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- *  ② 根据 username 获取数据库 user 信息
+ * ② 根据 username 获取数据库 user 信息
  */
 @Component
 public class SelfUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
         //构建用户信息的逻辑(取数据库/LDAP等用户信息)
         SelfUserDetails userInfo = new SelfUserDetails();
-        userInfo.setUsername(username); // 任意用户名登录
-
-        Md5PasswordEncoder md5PasswordEncoder = new Md5PasswordEncoder();
-        String encodePassword = md5PasswordEncoder.encodePassword("123", username); // 模拟从数据库中获取的密码原为 123
-        userInfo.setPassword(encodePassword);
+        userInfo.setUser(user);
 
         Set authoritiesSet = new HashSet();
         GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ADMIN"); // 模拟从数据库中获取用户角色
