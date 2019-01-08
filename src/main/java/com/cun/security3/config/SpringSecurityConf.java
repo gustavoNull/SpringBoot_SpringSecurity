46b0ff9662d2a29f3438ebe940492f724c22ce5f
gustavoNull <bigwanggang@163.com>
2019-01-08 12:23:44 +0800

    Update SpringSecurityConf.java

--- src/main/java/com/cun/security3/config/SpringSecurityConf.java
+++ src/main/java/com/cun/security3/config/SpringSecurityConf.java
@@ -1,11 +1,13 @@
 package com.cun.security3.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringSecurityConf extends WebSecurityConfigurerAdapter {
@@ -60,5 +62,10 @@ public class SpringSecurityConf extends WebSecurityConfigurerAdapter {
 
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler); // 无权访问 JSON 格式的数据
 
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
